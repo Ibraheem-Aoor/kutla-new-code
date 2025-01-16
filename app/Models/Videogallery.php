0c632e3755ce78ba1a813e488dc7bf95641677f1
Ibraheem <71406106+Ibraheem-Aoor@users.noreply.github.com>
2025-01-16 18:37:34 +0200

    update video image

--- app/Models/Videogallery.php
+++ app/Models/Videogallery.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Videogallery extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','description','video','video_source','video_option','status','user_id'];
+    protected $fillable = ['title','description','video','video_source','video_option','status','user_id' , 'image'];
 }
